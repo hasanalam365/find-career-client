6722d2fe8fc32fbff08853af5a94da53f628e260
Hasan Alam <hasanalam365@gmail.com>
2024-09-28 21:23:56 +0600

    project setup

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../shared/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../shared/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      <Outlet></Outlet>
+
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
