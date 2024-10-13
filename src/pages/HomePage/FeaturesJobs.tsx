fc028cba2d40fa57991ab47e6ec71c89eebfb71f
Hasan Alam <hasanalam365@gmail.com>
2024-10-13 07:04:24 +0600

    features section responsive

--- src/pages/HomePage/FeaturesJobs.tsx
+++ src/pages/HomePage/FeaturesJobs.tsx
@@ -13,22 +13,27 @@ const FeaturesJobs = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3 container mx-auto">
         <div className="flex gap-5 items-center justify-between bg-base-100 shadow-xl p-4 hover:border-b-2 hover:border-b-blue-600 hover:rounded-xl">
           <figure>
-            <SiTheboringcompany className="text-8xl ml-5" />
+            <SiTheboringcompany className="text-6xl md:text-6xl lg:text-8xl ml-5" />
           </figure>
 
           <div>
             <h3>Technical Database Engineer</h3>
             <h4 className="text-blue-600">Data Entry Management Company</h4>
-            <div className="flex items-center justify-center gap-5 mt-2">
+            <div className="flex items-center  gap-5 mt-2">
               <p className="bg-[#6ACA00] text-white text-sm px-2 py-1">
                 Full Time
               </p>
               <p>-</p>
               <p>Jamalpur</p>
             </div>
+            <div className="mt-3 flex items-center justify-end md:hidden lg:hidden">
+              <button className=" border-2 border-[#6ACA00] text-[#6ACA00] px-2 py-1 transition-colors duration-300 hover:text-white hover:bg-[#6ACA00]">
+                Apply Now
+              </button>
+            </div>
           </div>
           <div>
-            <button className=" border-2 border-[#6ACA00] text-[#6ACA00] px-2 py-1 transition-colors duration-300 hover:text-white hover:bg-[#6ACA00]">
+            <button className="hidden md:block lg:block border-2 border-[#6ACA00] text-[#6ACA00] px-2 py-1 transition-colors duration-300 hover:text-white hover:bg-[#6ACA00]">
               Apply Now
             </button>
           </div>
