0e21378521ab4e50961c5eda1f23f1c5009c004c
Hasan Alam <hasanalam365@gmail.com>
2024-11-27 15:33:36 +0600

    profile page design

--- src/pages/Dashboard/Profile.tsx
+++ src/pages/Dashboard/Profile.tsx
@@ -4,12 +4,23 @@ import useAuth from "../../hooks/useAuth";
 const Profile = () => {
   const { user } = useAuth();
 
+  console.log(user);
+
   return (
-    <div>
-      <ul>
-        <li> Name: {user?.displayName}</li>
-        <li> Email: {user?.email}</li>
-      </ul>
+    <div className="bg-[#f1f1f1]">
+      <div>
+        <div className="avatar">
+          <div className="w-24 rounded-xl">
+            <img src={user?.photoURL} />
+          </div>
+        </div>
+        <div>
+          <h3>{user?.displayName}</h3>
+          <h3>General Manager</h3>
+          <p>{user?.email}</p>
+          <p>0123456789</p>
+        </div>
+      </div>
     </div>
   );
 };
