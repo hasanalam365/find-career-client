80bf6147fbbc1b4fc1621d5da1bdd129b256ba1f
Hasan Alam <hasanalam365@gmail.com>
2024-12-01 18:14:08 +0600

    axiosSecure hooks done

--- src/hooks/useAxiosSecure.tsx
+++ src/hooks/useAxiosSecure.tsx
@@ -5,6 +5,29 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("token kitii");
+      config.headers.authorization = `Bearer ${token}`;
+
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  // Add a response interceptor
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
