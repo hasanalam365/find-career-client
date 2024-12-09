3823de1b48338b91e62ccc517d5985da7ce99f73
Hasan Alam <hasanalam365@gmail.com>
2024-12-09 11:24:05 +0600

    job id Link done

--- src/components/MyCirculars.tsx
+++ src/components/MyCirculars.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import useAxiosSecure from "../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../hooks/useAuth";
+import { ScaleLoader } from "react-spinners";
+import { Link } from "react-router-dom";
 
 const MyCirculars = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: myjobs = [] } = useQuery({
+  const { data: myjobs = [], isLoading } = useQuery({
     queryKey: ["my-job-data"],
     queryFn: async () => {
       const res = await axiosSecure.get(`/createJob/${user?.email}`);
@@ -19,35 +21,43 @@ const MyCirculars = () => {
 
   return (
     <div>
-      <div className="overflow-x-auto mt-5 rounded-lg ">
-        <table className="table table-zebra">
-          {/* head */}
-          <thead className="">
-            <tr className="bg-gray-600 text-white ">
-              <th>SL No.</th>
-              <th>Title</th>
-              <th>Sallary</th>
-              <th>Deadline</th>
-              <th>Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {myjobs.map((job, idx: number) => (
-              <tr
-                key={idx}
-                className={`${
-                  (idx + 1) / 2 === 0 ? "bg-gray-100" : "bg-white"
-                }`}>
-                <th>{idx + 1}</th>
-                <td>{job.jobTitle}</td>
-                <td>{job.salary}</td>
-                <td>{job.deadline}</td>
-                <td>VIEW</td>
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen">
+          <ScaleLoader />
+        </div>
+      ) : (
+        <div className="overflow-x-auto mt-5 rounded-lg ">
+          <table className="table table-zebra">
+            {/* head */}
+            <thead className="">
+              <tr className="bg-gray-600 text-white ">
+                <th>SL No.</th>
+                <th>Title</th>
+                <th>Sallary</th>
+                <th>Deadline</th>
+                <th>Details</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {myjobs.map((job, idx: number) => (
+                <tr
+                  key={idx}
+                  className={`${
+                    (idx + 1) / 2 === 0 ? "bg-gray-100" : "bg-white"
+                  }`}>
+                  <th>{idx + 1}</th>
+                  <td>{job.jobTitle}</td>
+                  <td>{job.salary}</td>
+                  <td>{job.deadline}</td>
+                  <td>
+                    <Link to={`/job/${job._id}`}>VIEW</Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
