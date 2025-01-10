41fb8b72c650edaedc7a5a7ac01f4f02412aeafb
Hasan Alam <hasanalam365@gmail.com>
2025-01-10 21:18:53 +0600

    registration page chante text

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -187,7 +187,7 @@ const Register = () => {
         <div className="flex items-center gap-1 justify-center mt-5">
           <p>Already have an account? Please</p>
           <Link to="/login" className="underline text-blue-600">
-            Login
+            Login here
           </Link>
         </div>
       </div>
