373d64e62a899b9ff83d99781bcf5544ae38264f
Hasan Alam <hasanalam365@gmail.com>
2025-01-15 20:46:34 +0600

    footer text change

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <nav>
           <h6 className="footer-title">Services</h6>
           <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
+         
           <a className="link link-hover">Marketing</a>
           <a className="link link-hover">Advertisement</a>
         </nav>
