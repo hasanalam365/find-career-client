8109ed3a09bbb4e9f3cd3b62f935ac605016ff97
Hasan Alam <hasanalam365@gmail.com>
2025-01-19 18:25:49 +0600

    footer chage

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -28,7 +28,6 @@ const Footer = () => {
           <h6 className="footer-title">Legal</h6>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
         </nav>
       </footer>
     </div>
