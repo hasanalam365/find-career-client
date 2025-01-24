10f39f167519f846fb18e3276333ea98d9627e0e
Hasan Alam <hasanalam365@gmail.com>
2025-01-24 22:50:11 +0600

    footer change 3

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
         </aside>
         <nav>
           <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
+         
 
           <a className="link link-hover">Marketing</a>
           <a className="link link-hover">Advertisement</a>
