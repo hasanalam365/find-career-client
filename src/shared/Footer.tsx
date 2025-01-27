22b852136c42bd6691966651c0d7375e2993b5dd
Hasan Alam <hasanalam365@gmail.com>
2025-01-27 16:03:39 +0600

    footer 6

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -13,17 +13,13 @@ const Footer = () => {
         </aside>
         <nav>
           <h6 className="footer-title">Services</h6>
-         
 
-      
           <a className="link link-hover">Advertisement</a>
         </nav>
         <nav>
-       
           <a className="link link-hover">Jobs</a>
         </nav>
         <nav>
-          <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
         </nav>
       </footer>
